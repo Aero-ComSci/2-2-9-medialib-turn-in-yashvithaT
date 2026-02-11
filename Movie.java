f96c252add8a4212024a427243cffbcedfc8d1e7
yashvithaT <148280511+yashvithaT@users.noreply.github.com>
2026-02-10 22:12:51 -0800

    Add Movie class with title and length attributes

---
+++ Movie.java
@@ -0,0 +1,8 @@
+public class Movie {
+    private String title;
+    private double length;
+    public Movie(String title, double length) {
+        this.title = title;
+        this.length = length;
+    }
+}
